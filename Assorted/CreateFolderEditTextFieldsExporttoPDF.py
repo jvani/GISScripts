56ecd84a4f2d07e2604631120df95d660f79f709
jvani <jvani@ualberta.ca>
2016-05-24 14:27:36 -0600

    Update and rename MXDTOPDF_2016VMP.py to CreateFolderEditTextFieldsExporttoPDF.py

--- Assorted/CreateFolderEditTextFieldsExporttoPDF.py
+++ Assorted/CreateFolderEditTextFieldsExporttoPDF.py
@@ -1,13 +1,14 @@
 import arcpy, time, os
 
 #Variables#
-folderName = "20160505"
-week = "15/05/2016 - 19/05/2016"
+folderName = "FOLDER_NAME"
+textelement_namme = "TEXTELEMENT_NAME"
+text_content = "TEXT_CONTENT"
 
 #Constants#
-MXDfolder = r"G:\Kearl\z2016\z16007\MXD"
+MXDfolder = r"MXD_PATH"
 MXDfolder=MXDfolder.replace("\\","\\\\")
-PDFfolder =  r"G:\Kearl\z2016\z16007\PDF"
+PDFfolder =  r"PDF_PATH"
 PDFfolder =PDFfolder.replace("\\","\\\\")
 
 ###Script###
@@ -25,7 +26,7 @@ for fn in os.listdir(MXDfolder ):
                 pdf =title[0:-4]
                 print pdf
                 for textElement in arcpy.mapping.ListLayoutElements(tempMXD, "TEXT_ELEMENT"):
-                    if textElement.name == "notes":
-                         textElement.text = "<und>NOTES:</und>"+"\r\n"+"\r\n"+"Imagery Date: September 2015"+"\r\n"+"\r\n"+"Reporting Period: "+week
+                    if textElement.name == textelement_name:
+                         textElement.text = text_content
                 tempMXD.save()
                 arcpy.mapping.ExportToPDF(tempMXD, PDFfolder+"\\"+folderName+"\\"+str(pdf))
